6c1daff68733b14bf0b4096aff744542c6043ca5
Connor Brown <falconnor4@gmail.com>
2025-12-18 20:33:15 -0800

    refactor: Rename NixOS configuration from 'nixos' to 'default'.

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
